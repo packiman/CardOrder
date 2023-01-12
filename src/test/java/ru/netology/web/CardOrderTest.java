e8491d73ab7fe5b3a2d924e1f8b47c8ef4c766ca
EvgenyKlishin <klishinev@gmail.com>
2023-01-12 18:59:48 +0300

    CI commit

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
